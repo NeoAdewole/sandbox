e59d889cee9909be465f41efb2d99badc857e83c
Matt Adewole <9janeo@gmail.com>
2023-09-18 18:37:07 -0500

    Adds webpack config to theme, enqueues index.js

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,6 +1,7 @@
 <?php
 
-function u_enqueue() {
+function u_enqueue()
+{
   wp_register_style(
     'u_font_rubik_and_pacifico',
     'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
@@ -13,10 +14,15 @@ function u_enqueue() {
   );
   wp_register_style(
     'u_theme',
-    get_theme_file_uri('assets/public/index.css')
+    get_theme_file_uri('build/index.css')
+  );
+  wp_register_script(
+    'u_theme',
+    get_theme_file_uri('build/index.js')
   );
 
   wp_enqueue_style('u_font_rubik_and_pacifico');
   wp_enqueue_style('u_bootstrap_icons');
   wp_enqueue_style('u_theme');
+  wp_enqueue_script('u_theme');
 }
