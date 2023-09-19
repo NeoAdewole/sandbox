b04689fc2000ee1777a3366378da9a00d37a007e
Matt Adewole <9janeo@gmail.com>
2023-09-18 22:00:42 -0500

    Watch for changes to html with browser-sync

--- src/browser-sync.config.cjs
+++ src/browser-sync.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   "proxy": "https://niyiadewole.local/",
   "notify": false,
-  "files": ["../css/*.min.css", "../js/*.min.js", "../**/*.php", "../theme.json"]
+  "files": ["../css/*.min.css", "../js/*.min.js", "../**/*.php", "../theme.json", "../**/*.html"]
 };
